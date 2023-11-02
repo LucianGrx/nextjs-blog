3b6eca2212c85f77eca8574f8ba35c9ace29d72e
luciangrx <luciangrr@gmail.com>
2023-11-02 11:11:12 +0200

    feat:Firt blog version

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Passionate about web development and creating captivating digital experiences. Let's create something amazing together! 😎👨‍💻</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
